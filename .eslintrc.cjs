68821c917e672dbaabcfc267423c2d375cd40c93
xlz122 <122874722@qq.com>
2024-06-13 15:32:47 +0800

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    '@typescript-eslint/no-explicit-any': 0,
+
+    // 禁止使用var
+    'no-var': [2],
+    // 禁止未使用的变量
+    '@typescript-eslint/no-unused-vars': [2],
+    // 字符串单引号
+    'quotes': [2, 'single'],
+    // 对象末尾逗号
+    'comma-dangle': [2, 'never'],
+    // 箭头函数前后空格
+    'arrow-spacing': [2, { before: true, after: true }],
+    // 箭头函数参数括号(1个参数不需要, 1个以上需要)
+    'arrow-parens': [2, 'as-needed'],
+    // 全等(===、!==)
+    'eqeqeq': [2],
+    // 语句结尾分号
+    'semi': [2, 'always'],
+    // 空格缩进
+    'indent': [2, 2],
+    // 单行代码最大长度
+    'max-len': [2, { code: 120 }],
+    // 文件末尾换行
+    'eol-last': [2],
+
+    // 检查 effect 的依赖
+    'react-hooks/exhaustive-deps': [0]
+  }
+};
---
+++ README.md
@@ -0,0 +1,34 @@
+# 荣耀官网
+
+## 环境要求
+
+需要 NodeJS 20.0.0+ 环境
+
+## 项目简介
+
+插件
+* vite-plugin-compression
+
+## 项目使用技术栈
+
+* React
+* Gsap
+* TypeScript
+
+## 运行
+
+启动项目
+
+```
+yarn install
+```
+
+```
+yarn dev
+```
+
+打包项目
+
+```
+yarn build
+```
