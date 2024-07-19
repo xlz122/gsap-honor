d440c0f475ef4927b03e7bb4fe37cd2ee2809ee0
xlz122 <122874722@qq.com>
2024-07-19 15:16:56 +0800

    feat: 完成第二屏

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './header/Header';
 import SectionHero from './section-hero/SectionHero';
+import SectionTvc from './section-tvc/SectionTvc';
 
 function App(): React.ReactElement {
   return (
     <>
       <Header />
       <SectionHero />
+      <SectionTvc />
     </>
   );
 }
