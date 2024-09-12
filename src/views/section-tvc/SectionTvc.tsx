eab4c6779ce006c71a68240b9d9f5c5b2473de0b
xlz122 <122874722@qq.com>
2024-09-12 08:57:49 +0800

    feat: 完成第二屏

---
+++ src/views/section-tvc/SectionTvc.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { getImageUrl } from '@/utils/utils';
+import './section-tvc.scss';
+
+function SectionTvc(): React.ReactElement {
+  return (
+    <section className="section-tvc">
+      <div className="section-wrapper">
+        <video className="video" autoPlay playsInline loop muted>
+          <source src={getImageUrl('assets/section-tvc/tvc-glance.mp4')} />
+        </video>
+      </div>
+    </section>
+  );
+}
+
+export default SectionTvc;
